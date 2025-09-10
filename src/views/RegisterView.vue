a437b060ddbdd7daf0e511a7bd01bfeb3d91e4f4
meraki6512 <jweun123@gmail.com>
2025-09-10 09:59:52 +0900

    fix: http 프로토콜로 인한 가입 시 비밀번호 유출 검사 문제 일시적 해결
    - 회원가입 폼의 password 입력 필드에 autocomplete="new-password" 속성을 추가

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -91,7 +91,7 @@ const onRegister = async () => {
               v-model="password" 
               name="password" 
               type="password" 
-              autocomplete="current-password" 
+              autocomplete="new-password" 
               required 
               class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-green-500 focus:outline-none focus:ring-green-500 sm:text-sm" 
               placeholder="Password"
